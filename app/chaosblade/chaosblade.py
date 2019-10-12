3982f695f3572fc107e6d8eb0c80542de39bea4b
baiyanquan <405579949@qq.com>
2019-10-12 15:16:30 +0800

    no message

--- app/chaosblade/chaosblade.py
+++ app/chaosblade/chaosblade.py
@@ -6,28 +6,70 @@ from services.fault_injector import FaultInjector
 chaosblade = Blueprint('chaosblade', __name__)
 
 
-@chaosblade.route('/tool/api/v1.0/chaosblade-vm/inject', methods=['POST'])
-def chaos_injectvm():
-    if not request.json or 'type' not in request.json or 'host' not in request.json:
+@chaosblade.route('/tool/api/v1.0/chaosblade/inject-cpu', methods=['POST'])
+def chaos_inject_cpu():
+    if not request.json or 'host' not in request.json:
         abort(400)
     dto = {
-        'type': request.json['type'],
-        # 'inject_duration': request.json['duration']
-        'host': request.json['host']
+        'host': request.json['host'],
     }
-    return jsonify(FaultInjector.chaos_inject_vm(dto))
+    return jsonify(FaultInjector.chaos_inject_cpu(dto))
 
 
-@chaosblade.route('/tool/api/v1.0/chaosblade-vm/stop-specific-inject', methods=['POST'])
+@chaosblade.route('/tool/api/v1.0/chaosblade/inject-mem', methods=['POST'])
+def chaos_inject_mem():
+    if not request.json or 'host' not in request.json or 'percent' not in request.json:
+        abort(400)
+    dto = {
+        'host': request.json['host'],
+        'percent': request.json['percent']
+    }
+    return jsonify(FaultInjector.chaos_inject_mem(dto))
+
+
+@chaosblade.route('/tool/api/v1.0/chaosblade/inject-disk', methods=['POST'])
+def chaos_inject_disk():
+    if not request.json or 'host' not in request.json or 'type' not in request.json:
+        abort(400)
+    dto = {
+        'host': request.json['host'],
+        'type': request.json['type']
+    }
+    return jsonify(FaultInjector.chaos_inject_disk(dto))
+
+
+@chaosblade.route('/tool/api/v1.0/chaosblade/inject-network', methods=['POST'])
+def chaos_inject_network():
+    if not request.json or 'host' not in request.json or 'time' not in request.json or 'timeout' not in request.json:
+        abort(400)
+    dto = {
+        'host': request.json['host'],
+        'time': request.json['time'],
+        'timeout': request.json['timeout']
+    }
+    return jsonify(FaultInjector.chaos_inject_network(dto))
+
+
+@chaosblade.route('/tool/api/v1.0/chaosblade/inject-random', methods=['POST'])
+def chaos_inject_random():
+    if not request.json or 'host' not in request.json:
+        abort(400)
+    dto = {
+        'host': request.json['host'],
+    }
+    return jsonify(FaultInjector.chaos_inject_random(dto))
+
+
+@chaosblade.route('/tool/api/v1.0/chaosblade/stop-specific-inject', methods=['POST'])
 def stop_specific_inject():
     if not request.json or 'tag' not in request.json:
         abort(400)
     dto = {
         'tag': request.json['tag'],
     }
-    return jsonify(FaultInjector.stop_chaosinject_vm(dto))
+    return jsonify(FaultInjector.stop_chaos_inject(dto))
 
 
-@chaosblade.route('/tool/api/v1.0/chaosblade-vm/view-inject-info', methods=['GET'])
+@chaosblade.route('/tool/api/v1.0/chaosblade/view-inject-info', methods=['GET'])
 def view_inject_info():
-    return jsonify(FaultInjector.view_chaos_inject_vm())
+    return jsonify(FaultInjector.view_chaos_inject())
--- services/fault_injector.py
+++ services/fault_injector.py
@@ -3,31 +3,42 @@
 import sys
 import random
 import json
-import ast
+import requests
 from utils.ansible_runner import Runner
+from requests.exceptions import Timeout
 
 sys.path.append('../')
 
+target_url = "http://192.168.199.236:8080/reasoner/message"
+
 Hosts = [
-    "192.168.199.31",
-    "192.168.199.32",
-    "192.168.199.33",
-    "192.168.199.34",
-    "192.168.199.35"
+    "192.168.199.41",
+    "192.168.199.42",
+    "192.168.199.43",
+    "192.168.199.44",
+    "192.168.199.45"
 ]
 
-HostsVM = [
-    "192.168.199.21",
-    "192.168.199.22",
-    "192.168.199.23",
-    "192.168.199.24",
-    "192.168.199.25"
-]
+Spare_hosts = {
+    "192.168.199.41": "192.168.199.31",
+    "192.168.199.42": "192.168.199.32",
+    "192.168.199.43": "192.168.199.33",
+    "192.168.199.44": "192.168.199.34",
+    "192.168.199.45": "192.168.199.35"
+}
+
+Default_cmd = {
+    "cpu": "./blade create cpu fullload",
+    "network": "./blade create network delay --interface enp3s0 --time 1000 --timeout 600",
+    "disk": "./blade create disk burn --read",
+    "mem": "./blade create mem load --mem-percent 80"
+}
 
 Cmd = {
-    "cpu": "blade create cpu fullload",
-    "network": "blade create network delay --interface ens160",
-    "disk": "blade create disk fill --size 1000"
+    "cpu": "./blade create cpu fullload",
+    "network": "./blade create network delay --interface enp3s0 ",
+    "disk": "./blade create disk burn --",
+    "mem": "./blade create mem load --mem-percent "
 }
 
 inject_info = []
@@ -38,79 +49,15 @@ class FaultInjector(object):
         pass
 
     @staticmethod
-    def inject_cpu(dto):
-        r = Runner()
-        r.run_ad_hoc(
-            hosts=dto['host'],
-            module='shell',
-            args='stress -c 1 -t ' + dto['inject_duration'] + ' > /dev/null 2>&1'
-        )
-        result = r.get_adhoc_result()
-        return result
-
-    @staticmethod
-    def inject_mem(dto):
-        r = Runner()
-        r.run_ad_hoc(
-            hosts=dto['host'],
-            module='shell',
-            args='stress --vm 4 --vm-bytes 1G --vm-hang ' + dto['inject_duration'] + ' -t '
-                 + dto['inject_duration'] + ' > /dev/null 2>&1'
-        )
-        result = r.get_adhoc_result()
-        return result
-
-    @staticmethod
-    def inject_io(dto):
-        r = Runner()
-        r.run_ad_hoc(
-            hosts=dto['host'],
-            module='shell',
-            args='stress -i 100 -t ' + dto['inject_duration'] + ' > /dev/null 2>&1'
-        )
-        result = r.get_adhoc_result()
-        return result
-
-    @staticmethod
-    def chaosinject(dto):
-        i=random.randint(0, 4)
-        j=random.randint(0, 2)
-        r = Runner()
-        r.run_ad_hoc(
-            hosts=Hosts[i],
-            module='shell',
-            args=Cmd[j]+" --timeout " + dto['inject_duration']
-        )
-        result = r.get_adhoc_result()
-        return result
-
-    @staticmethod
-    def chaosinject1(dto):
-        #i=random.randint(0, 4)
-        #j=random.randint(0, 2)
-        r = Runner()
-        r.run_ad_hoc(
-            hosts="192.168.199.31",
-            module='shell',
-            args="blade create disk fill --size 1000 --timeout " + dto['inject_duration']
-        )
-        result = r.get_adhoc_result()
-        return result
-
-    @staticmethod
-    def chaos_inject_vm(dto):
+    def chaos_inject_cpu(dto):
         target_inject = ''
         target_host = ''
         if dto['host'] == 'random':
-            i = random.randint(0, len(HostsVM)-1)
-            target_host = HostsVM[i]
+            i = random.randint(0, len(Hosts) - 1)
+            target_host = Hosts[i]
         else:
             target_host = dto['host']
-        if dto['type'] == 'random':
-            j = random.randint(0, len(Cmd)-1)
-            target_inject = Cmd[Cmd.keys()[j]]
-        else:
-            target_inject = Cmd[dto['type']]
+        target_inject = Cmd["cpu"]
         r = Runner()
         r.run_ad_hoc(
             hosts=target_host,
@@ -121,21 +68,188 @@ class FaultInjector(object):
         if len(result["success"]) > 0:
             transform_ip = result["success"].keys()[0]
             the_inject_info = {
-                "position": target_inject,
+                "position": "cpu",
                 "ip": target_host,
                 "start_time": result["success"][transform_ip]["start"],
                 "cmd": result["success"][transform_ip]["cmd"],
-                "cmd_id": json.loads(result["success"][transform_ip]["stderr"].encode('unicode-escape').decode('string_escape'))["result"].encode('unicode-escape').decode('string_escape')
+                "cmd_id": json.loads(
+                    result["success"][transform_ip]["stdout"].encode('unicode-escape').decode('string_escape'))[
+                    "result"].encode('unicode-escape').decode('string_escape')
             }
             inject_info.append(the_inject_info)
+            try:
+                requests.post(url=target_url, params={"content": str(the_inject_info)}, verify=False, timeout=2)
+            except Timeout:
+                pass
+            finally:
+                return result
         return result
 
     @staticmethod
-    def view_chaos_inject_vm():
+    def chaos_inject_mem(dto):
+        target_inject = ''
+        target_host = ''
+        if dto['host'] == 'random':
+            i = random.randint(0, len(Hosts) - 1)
+            target_host = Hosts[i]
+        else:
+            target_host = dto['host']
+        if dto['percent'] == 'default':
+            target_inject = Default_cmd["mem"]
+        else:
+            target_inject = Cmd["mem"] + dto['percent']
+        r = Runner()
+        r.run_ad_hoc(
+            hosts=target_host,
+            module='shell',
+            args=target_inject
+        )
+        result = r.get_adhoc_result()
+        if len(result["success"]) > 0:
+            transform_ip = result["success"].keys()[0]
+            the_inject_info = {
+                "position": "mem",
+                "ip": target_host,
+                "start_time": result["success"][transform_ip]["start"],
+                "cmd": result["success"][transform_ip]["cmd"],
+                "cmd_id": json.loads(
+                    result["success"][transform_ip]["stdout"].encode('unicode-escape').decode('string_escape'))[
+                    "result"].encode('unicode-escape').decode('string_escape')
+            }
+            inject_info.append(the_inject_info)
+            try:
+                requests.post(url=target_url, params={"content": str(the_inject_info)}, verify=False, timeout=2)
+            except Timeout:
+                pass
+            finally:
+                return result
+        return result
+
+    @staticmethod
+    def chaos_inject_disk(dto):
+        target_inject = ''
+        target_host = ''
+        if dto['host'] == 'random':
+            i = random.randint(0, len(Hosts) - 1)
+            target_host = Hosts[i]
+        else:
+            target_host = dto['host']
+        if dto['type'] == 'default':
+            target_inject = Default_cmd["disk"]
+        else:
+            target_inject = Cmd["disk"] + dto['type']
+        r = Runner()
+        r.run_ad_hoc(
+            hosts=target_host,
+            module='shell',
+            args=target_inject
+        )
+        result = r.get_adhoc_result()
+        if len(result["success"]) > 0:
+            transform_ip = result["success"].keys()[0]
+            the_inject_info = {
+                "position": "disk",
+                "ip": target_host,
+                "start_time": result["success"][transform_ip]["start"],
+                "cmd": result["success"][transform_ip]["cmd"],
+                "cmd_id": json.loads(
+                    result["success"][transform_ip]["stdout"].encode('unicode-escape').decode('string_escape'))[
+                    "result"].encode('unicode-escape').decode('string_escape')
+            }
+            inject_info.append(the_inject_info)
+            try:
+                requests.post(url=target_url, params={"content": str(the_inject_info)}, verify=False, timeout=2)
+            except Timeout:
+                pass
+            finally:
+                return result
+        return result
+
+    @staticmethod
+    def chaos_inject_network(dto):
+        target_inject = ''
+        target_host = ''
+        time = '3000'
+        timeout = '600'
+        if dto['host'] == 'random':
+            i = random.randint(0, len(Hosts) - 1)
+            target_host = Hosts[i]
+        else:
+            target_host = dto['host']
+        if dto['time'] != 'default':
+            time = dto['time']
+        if dto['timeout'] != 'default':
+            timeout = dto['timeout']
+        target_inject = Cmd['network'] + '--time ' + time + ' --timeout ' + timeout
+        r = Runner()
+        r.run_ad_hoc(
+            hosts=target_host,
+            module='shell',
+            args=target_inject
+        )
+        result = r.get_adhoc_result()
+        if len(result["success"]) > 0:
+            transform_ip = result["success"].keys()[0]
+            the_inject_info = {
+                "position": "network",
+                "ip": target_host,
+                "start_time": result["success"][transform_ip]["start"],
+                "cmd": result["success"][transform_ip]["cmd"],
+                "cmd_id": json.loads(
+                    result["success"][transform_ip]["stdout"].encode('unicode-escape').decode('string_escape'))[
+                    "result"].encode('unicode-escape').decode('string_escape')
+            }
+            inject_info.append(the_inject_info)
+            try:
+                requests.post(url=target_url, params={"content": str(the_inject_info)}, verify=False, timeout=2)
+            except Timeout:
+                pass
+            finally:
+                return result
+        return result
+
+    @staticmethod
+    def chaos_inject_random(dto):
+        target_inject = ''
+        target_host = ''
+        if dto['host'] == 'random':
+            i = random.randint(0, len(Hosts)-1)
+            target_host = Hosts[i]
+        else:
+            target_host = dto['host']
+        j = random.randint(0, len(Cmd) - 1)
+        target_inject = Default_cmd[Default_cmd.keys()[j]]
+        r = Runner()
+        r.run_ad_hoc(
+            hosts=target_host,
+            module='shell',
+            args=target_inject
+        )
+        result = r.get_adhoc_result()
+        if len(result["success"]) > 0:
+            transform_ip = result["success"].keys()[0]
+            the_inject_info = {
+                "position": Default_cmd.keys()[j],
+                "ip": target_host,
+                "start_time": result["success"][transform_ip]["start"],
+                "cmd": result["success"][transform_ip]["cmd"],
+                "cmd_id": json.loads(result["success"][transform_ip]["stdout"].encode('unicode-escape').decode('string_escape'))["result"].encode('unicode-escape').decode('string_escape')
+            }
+            inject_info.append(the_inject_info)
+            try:
+                requests.post(url=target_url, params={"content": str(the_inject_info)}, verify=False, timeout=2)
+            except Timeout:
+                pass
+            finally:
+                return result
+        return result
+
+    @staticmethod
+    def view_chaos_inject():
         return inject_info
 
     @staticmethod
-    def stop_chaosinject_vm(dto):
+    def stop_chaos_inject(dto):
         stop_id = dto['tag']
         find = 0
         key = 0
@@ -143,17 +257,32 @@ class FaultInjector(object):
             if inject_info[i]["cmd_id"] == stop_id:
                 find = 1
                 key = i
-        a = inject_info[key]["ip"]
         if find == 1:
             r = Runner()
             r.run_ad_hoc(
-                hosts=inject_info[key]["ip"],
+                hosts=Spare_hosts[inject_info[key]["ip"]],
                 module='shell',
-                args="blade destroy " + stop_id
+                args="./blade destroy " + stop_id
             )
-            inject_info.pop(key)
             result = r.get_adhoc_result()
-            return result
+            if len(result["success"]) > 0:
+                transform_ip = result["success"].keys()[0]
+                the_stop_info = {
+                    "position": inject_info[key]["position"],
+                    "ip": Spare_hosts[inject_info[key]["ip"]],
+                    "start_time": result["success"][transform_ip]["start"],
+                    "cmd": result["success"][transform_ip]["cmd"],
+                }
+                try:
+                    requests.post(url=target_url, params={"content": str(the_stop_info)}, verify=False, timeout=2)
+                except Timeout:
+                    pass
+                finally:
+                    inject_info.pop(key)
+                    return result
+            else:
+                inject_info.pop(key)
+                return result
         else:
             return 'Inject not found'
 
