a19ad94720cca852f6e84990680d7679f7630839
Kowalski Dark <darkkowalski2012@gmail.com>
2020-11-07 17:41:03 +0800

    do a big rewrite

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/code/chown.sh `id -u` `id -g` /code mypasswd
+
+if [ $? -ne 0 ]; then
+  return -1
+fi
+
+flask run --host=0.0.0.0 --port 8080
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/ChaosToolbox
-git pull
-python ./manage.py
