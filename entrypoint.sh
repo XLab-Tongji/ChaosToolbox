ba26630b670462208d4f4b9968d39b0c5d2c1a43
lwq <1853401@tongji.edu.cn>
2020-11-08 20:05:58 +0800

    support ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
 /code/chown.sh `id -u` `id -g` /code mypasswd
-
 if [ $? -ne 0 ]; then
+  echo "failed to chown /code"
+  return -1
+fi
+
+/code/chown.sh `id -u` `id -g` /home/code mypasswd
+if [ $? -ne 0 ]; then
+  echo "failed to chown /home/code"
+  return -1
+fi
+
+chmod 600 /home/code/.ssh/id_rsa
+if [ $? -ne 0 ]; then
+  ehco "failed to chmod 600 /home/code/.ssh/id_rsa"
+  return -1
+fi
+
+chmod 644 /home/code/.ssh/id_rsa.pub
+if [ $? -ne 0 ]; then
+  echo "failed to chmod 644 /home/code/.ssh/id_rsa.pub"
+  return -1
+fi
+
+chmod 700 /home/code/.ssh
+if [ $? -ne 0 ]; then
+  echo "failed to chmod 700 /home/code/.ssh"
+  return -1
+fi
+
+/code/useradd.sh `id -u` `id -g` code mypasswd
+if [ $? -ne 0 ]; then
+  echo "failed to useradd code"
   return -1
 fi
 
